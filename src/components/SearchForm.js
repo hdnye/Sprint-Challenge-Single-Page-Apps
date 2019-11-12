e6709eefe4d1750775189e3f5a9ea7b12fc39ea8
Heather Nye <heatherdnye@gmail.com>
2019-11-11 19:28:28 -0600

    search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from "react";
+import axios from 'axios';
 import { Form, Field, withFormik } from 'formik';
 
 
-export default function SearchForm({ errors, touched, status }) {
+export default function SearchForm({ errors, touched, value }) {
  
   const [char, setChar] = useState([]);
-  
+  const [query, setQuery] = useState('');
+
   useEffect(() => {
-    status && setChar(char => [...char, status])
-     }, {status});
+    axios.get(`https://ickandmortyapi.com/api/character/`)
+      .then(response => {
+        const character = response.data.results.filter(char = char.name.toLowerCase().includes(query.toLowerCase()));
+        setChar(character);
+      });
+    }, [query]);
+
+const handleInputChange = (event) => {
+    setQuery(event.target.value);
+}
 
   return (
     <section className="search-form">
      {/* Add a search form here */}
-    <hi1>Search Form</hi1>
+    <h1>Search Form</h1>
     <Form>
-      <Field type='text' name='name' placeholder='Name' />
+      {/* Enter character search field */}
+      <Field 
+        type='text' 
+        name='name'
+        placeholder='Name' 
+        value={query} onChange={handleInputChange} />
         {touched.name && errors.name && ( 
           <p className='error'>{errors.name}</p>
         )}
