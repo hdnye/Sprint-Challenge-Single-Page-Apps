dfdfc0897adbabd07e0ba58d8f0dcbe91ed52e20
Heather Nye <heatherdnye@gmail.com>
2019-11-11 19:16:01 -0600

    added search form

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -12,8 +12,9 @@ export default function CharacterList() {
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
     
     axios
-      .get('https:rickandmortyapi.com/api/character/')
+      .get(`https:rickandmortyapi.com/api/character/`)
         .then(response => {
+          console.log(response);
           setChar(response.data)
          })
         .catch(error => {
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Form, Field, withFormik } from 'formik';
 
-export default function SearchForm() {
+
+export default function SearchForm({ errors, touched, status }) {
  
+  const [char, setChar] = useState([]);
+  
+  useEffect(() => {
+    status && setChar(char => [...char, status])
+     }, {status});
+
   return (
     <section className="search-form">
      {/* Add a search form here */}
-    </section>
+    <hi1>Search Form</hi1>
+    <Form>
+      <Field type='text' name='name' placeholder='Name' />
+        {touched.name && errors.name && ( 
+          <p className='error'>{errors.name}</p>
+        )}
+     <button type='submit'>Submit</button>
+    </Form>
+   </section>
   );
 }
